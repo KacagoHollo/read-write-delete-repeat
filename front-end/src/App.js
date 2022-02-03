3bb9557fee84b9820e488ed9aee7ccf89577e96b
IstvanPongracz <pongracz88@gmail.com>
2022-02-03 01:07:46 +0100

    "List all tasks" part completed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+// import axios from 'axios'
+
+const axios = require('axios').default;
 
 function App() {
+
+  const [manageTask, setManageTask] = useState([]);
+  const [addTask, setAddTask] = useState("");
+
+  const getData = async () => {
+    const response = await axios.get('http://localhost:6789/tasks/');
+    setManageTask(response.data)
+   
+  }
+  // console.log(getData());
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(manageTask);
+  
   const createTask = () => {
+    setAddTask([...addTask])
   };
 
   const deleteTask = (id) => {
@@ -12,19 +31,25 @@ function App() {
     <>
         <h1>ToDo List</h1>
         <div id="menu">
-            <label>Your new to-do</label>
-            <input type="text" placeholder="enter new task"/>
-            <button onClick={createTask}>add task</button>
+          <label>Your new to-do</label>
+          <input type="text" placeholder="enter new task"/>
+          <button onClick={createTask}>add task</button>
         </div>
         <div id="list">
-            <div>
-                <button onClick={() => deleteTask(1)}>delete</button>
-                <li style={{ display: 'inline' }}>
-                      buy milk
-                </li>
-                <br></br>
-            </div>
+          <button onClick={() => deleteTask(1)}>delete</button>
+          <li style={{ display: 'inline' }}>
+                buy milk
+          </li>
+          <br></br>
         </div>
+        {manageTask.map((task, id) => (
+          <div key={id}>
+            <ul>
+              <li>{task.title}</li>
+            </ul>
+          </div>
+        ))
+        }
     </>
   );
 }
